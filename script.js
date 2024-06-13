fabfdefc0ffaa9b3108142b5820c1abe5765eadb
ahmeteid <ahmeteid355@gmail.com>
2024-06-13 16:05:16 +0300

    Added validate (first name, last name, email & teatarea) to make there required

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let fName = document.querySelector("#f-name");
 let lName = document.querySelector("#l-name");
 let email = document.querySelector("#e-mail");
+let textArea = document.querySelector("#m-essage");
 
 document.forms[0].onsubmit = function (e) {
   let fNameValid = false;
   let lNameValid = false;
   let emailValid = false;
+  let textAreaValid = false;
 
-  console.log(fName.value);
-  console.log(fName.value.length);
-
-  if (fName.value !== "" && lName.value !== "" && email.value !== "") {
+  if (
+    fName.value !== "" &&
+    lName.value !== "" &&
+    email.value !== "" &&
+    textArea.value !== ""
+  ) {
     fNameValid = true;
     lNameValid = true;
     emailValid = true;
+    textAreaValid = true;
   }
 
   if (
-    fNameValid.value === false ||
-    lNameValid.value === false ||
-    emailValid === false
+    fNameValid === false ||
+    lNameValid === false ||
+    emailValid === false ||
+    textAreaValid === false
   ) {
     e.preventDefault();
   }
